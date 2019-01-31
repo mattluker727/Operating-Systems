542dcc2ee98a03632cedc78f716580ec434f1480
mattluker727 <mattluker727@gmail.com>
2019-01-31 15:52:04 -0500

    update

--- Project 1/context_switch.c
+++ Project 1/context_switch.c
@@ -79,7 +79,7 @@
 			else{							/* Parent writes pipeText to pipe */
 				printf("P: %d\n",sched_getcpu());
 				close(pipefd[0]);			/* Close unused read end */
-				write(pipefd[1], pipeText, strlen(pipeText));
+				write(pipefd[1], buf, strlen(pipeText));
 				close(pipefd[1]);			/* Reader will see EOF */
 
 				wait(NULL);					/* Wait for child */
